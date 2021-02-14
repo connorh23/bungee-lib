fb80daf80acd06a4f478d1fc0ac6e4b5d0e7a56c
Connor Hood <connor.hood234@mail.com>
2021-02-14 02:35:06 -0800

    Streamline async in requests.js

--- requests.js
+++ requests.js
@@ -5,43 +5,35 @@ const {
 } = import('./core.js');
 
 const get = async({ url, query_params, request_headers }) => {
-   return await execute_with_retry(async () => {
+   return execute_with_retry(async () => {
       axios.get(url, {
          ... query_params && { params: query_params },
          ... request_headers && { headers: request_headers },
-      }).then(data => {
-         return data;
-      }).catch(e => throw e);
+      });
    });
 };
 
 const post = async({ url, request_body={}, request_headers }) => {
-   return await execute_with_retry(async () => {
+   return execute_with_retry(async () => {
       axios.post(url, request_body, {
          ... request_headers && { headers: request_headers }
-      }).then(data => {
-         return data;
-      }).catch(e => throw e);
+      });
    });
 };
 
 const put = async({ url, request_body, request_headers }) => {
-   return await execute_with_retry(async () => {
+   return execute_with_retry(async () => {
       axios.put(url, request_body, {
          ... request_headers && { headers: request_headers }
-      }).then(data => {
-         return data;
-      }).catch(e => throw e);
+      });
    });
 };
 
 const destroy = async({ url, request_headers }) => {
-   return await execute_with_retry(async () => {
+   return execute_with_retry(async () => {
       return axios.delete(url, {
          ...request_headers && {headers: request_headers},
-      }).then(data => {
-         return data;
-      }).catch(e => throw e);
+      });
    });
 };
 
