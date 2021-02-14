08256675cd56f0ee7a7b8094e1ff2808c3555eab
Connor Hood <connor.hood234@mail.com>
2021-02-14 12:45:27 -0800

    Import fix - version 1.0.1

--- http/requests.js
+++ http/requests.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
 const {
-   execute_with_retry
-} = require('./lib/asnyc_executor.js');
+   async_executor
+} = require('../util')
 
 const get = async({ url, query_params, request_headers }) => {
-   return execute_with_retry(async () => {
+   return async_executor.execute_with_retry(async () => {
       return axios.get(url, {
          ... query_params && { params: query_params },
          ... request_headers && { headers: request_headers },
@@ -14,7 +14,7 @@ const get = async({ url, query_params, request_headers }) => {
 };
 
 const post = async({ url, request_body={}, request_headers }) => {
-   return execute_with_retry(async () => {
+   return async_executor.execute_with_retry(async () => {
       return  axios.post(url, request_body, {
          ... request_headers && { headers: request_headers }
       });
@@ -22,7 +22,7 @@ const post = async({ url, request_body={}, request_headers }) => {
 };
 
 const put = async({ url, request_body, request_headers }) => {
-   return execute_with_retry(async () => {
+   return async_executor.execute_with_retry(async () => {
       return axios.put(url, request_body, {
          ... request_headers && { headers: request_headers }
       });
@@ -30,7 +30,7 @@ const put = async({ url, request_body, request_headers }) => {
 };
 
 const destroy = async({ url, request_headers }) => {
-   return execute_with_retry(async () => {
+   return async_executor.execute_with_retry(async () => {
       return axios.delete(url, {
          ...request_headers && { headers: request_headers },
       });
