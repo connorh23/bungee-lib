d7ad152eba21ba2e0a073791ff6e115a25b293f1
Connor Hood <connor.hood234@mail.com>
2021-02-20 14:57:36 -0800

    Updated module structure.  Don't export low-level modules from top level.

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-const requests = require('./http/requests');
-const responses = require('./http/responses');
+
+const http = require('./http');
 const environment = require('./environment');
 
 module.exports = {
    environment,
-   requests,
-   responses
+   http,
 };
-
-const test = async () => {
-
-   let response = await requests.get({
-      url: 'https://bg-data.tavour.com/rest/beers?page=1&pageSize=10'
-   });
-
-   console.log(response);
-}
-
-test();
