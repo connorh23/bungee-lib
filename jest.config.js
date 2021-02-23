cd49a088176770d303d5f0005432033bceae637e
Connor Hood <connor.hood234@mail.com>
2021-02-23 14:44:43 -0800

    Initial commit of jest config

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+
+   // Automatically clear mock calls and instances between every test
+   clearMocks: true,
+
+   collectCoverage: true,
+   coverageDirectory: "coverage",
+   coveragePathIgnorePatterns: [
+
+      // Standard ignorePatterns
+      "/__test__/",
+      "/node_modules/",
+      "./jest.config.js",
+      ".eslintrc.js",
+
+      // Unit test not necessary
+      "/tbdb/api.js",
+      "scripts/sequelize"
+
+
+   ],
+   coverageThreshold: {
+      global: {
+         statements: 80,
+         branches: 70,
+         lines: 70,
+         functions: 70,
+      }
+   },
+
+
+   testMatch: [
+      "**/__test__/**/*.test.[jt]s?(x)",
+      "**/?(*.)+(spec|test).[tj]s?(x)"
+   ],
+};
