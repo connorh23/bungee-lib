4503ac84b956537f460e3bc404f9dd7a2c86dec4
Connor Hood <connor.hood234@mail.com>
2021-02-23 23:01:49 -0800

    Update async_executor unit tests to use new format and test for telemetry data

--- __test__/util/async_executor.test.js
+++ __test__/util/async_executor.test.js
@@ -1,13 +1,28 @@
 const { async_executor } = require('../../util');
 
-test("async_executor.execute returns the response of successful method invocation", async () => {
+const timeout = async (ms) => {
+   return new Promise(resolve => setTimeout(resolve, ms));
+};
+const TEST_TIMEOUT = 1;
+const TEST_RETURN_VALUE = 'test';
 
-   const test_return_value = 'test';
-   const test_method = jest.fn(async () => { return test_return_value });
+test("async_executor.execute returns the response of successful method invocation with telemetry", async () => {
 
-   const response = await async_executor.execute_with_retry(test_method);
-   expect(response).toEqual(test_return_value);
+   const test_method = jest.fn(async () => {
+      await timeout(TEST_TIMEOUT);
+      return TEST_RETURN_VALUE
+   });
 
+   const response = await async_executor.execute({
+      method: test_method,
+      collect_telemetry: true,
+      num_retries: 3
+   });
+
+   expect(response.response).toEqual(TEST_RETURN_VALUE);
+
+   expect(response.telemetry).toBeDefined();
+   verify_telemetry_metrics({ response, test_timeout: TEST_TIMEOUT });
 });
 
 test("async_executor.execute retries failed method for specified number of retries", async () => {
@@ -18,7 +33,10 @@ test("async_executor.execute retries failed method for specified number of retri
    let error_thrown = false;
    let response;
    try {
-      await async_executor.execute_with_retry(test_method, num_retries);
+      await async_executor.execute({
+         method: test_method,
+         num_retries
+      });
    } catch (err) {
       error_thrown = true;
    }
@@ -26,23 +44,33 @@ test("async_executor.execute retries failed method for specified number of retri
    expect(error_thrown).toEqual(true);
    expect(response).toBeUndefined();
    expect(test_method).toHaveBeenCalledTimes(num_retries);
-
 });
 
-test("async_executor.execute retries defaults to 3 reties", async () => {
+test("async_executor.execute defaults to 0 reties", async () => {
 
    const test_method = jest.fn(async () => { throw new Error("error")});
 
    let error_thrown = false;
    let response;
    try {
-      await async_executor.execute_with_retry(test_method);
+      await async_executor.execute({
+         method: test_method
+      });
    } catch (err) {
       error_thrown = true;
    }
 
    expect(error_thrown).toEqual(true);
    expect(response).toBeUndefined();
-   expect(test_method).toHaveBeenCalledTimes(3);
+   expect(test_method).toHaveBeenCalledTimes(1);
 
 });
+
+const verify_telemetry_metrics = ({ response, test_timeout }) => {
+   expect(response.telemetry).toBeDefined();
+   const telemetry_data = response.telemetry;
+   const start = telemetry_data.start;
+   const end = telemetry_data.end;
+   expect(start <= end).toEqual(true);
+   expect(telemetry_data.latency).toBeGreaterThanOrEqual(test_timeout);
+};
