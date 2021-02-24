c8119414bb82b7ee0127bbcb5cb007473b1f490b
Connor Hood <connor.hood234@mail.com>
2021-02-23 23:00:52 -0800

    Refactor http/requests to use new async_executor format

--- http/requests.js
+++ http/requests.js
@@ -1,39 +1,49 @@
 const axios = require('axios');
+const { async_executor } = require('../util');
 
-const {
-   async_executor
-} = require('../util')
-
-const get = async({ url, query_params, request_headers }) => {
-   return async_executor.execute_with_retry(async () => {
-      return axios.get(url, {
-         ... query_params && { params: query_params },
-         ... request_headers && { headers: request_headers },
-      });
+const get = async({ url, query_params, request_headers, num_retries }) => {
+   return async_executor.execute({
+      method: async () => {
+         return axios.get(url, {
+            ...query_params && {params: query_params},
+            ...request_headers && {headers: request_headers},
+         });
+      },
+      num_retries
    });
 };
 
-const post = async({ url, request_body={}, request_headers }) => {
-   return async_executor.execute_with_retry(async () => {
-      return  axios.post(url, request_body, {
-         ... request_headers && { headers: request_headers }
-      });
+const post = async({ url, request_body={}, request_headers, num_retries }) => {
+   return async_executor.execute({
+      method: async () => {
+         return axios.post(url, request_body, {
+            ...request_headers && {headers: request_headers}
+         });
+      },
+      num_retries
    });
 };
 
-const put = async({ url, request_body, request_headers }) => {
-   return async_executor.execute_with_retry(async () => {
-      return axios.put(url, request_body, {
-         ... request_headers && { headers: request_headers }
-      });
+
+const put = async({ url, request_body, request_headers, num_retries }) => {
+   return async_executor.execute({
+      method: async () => {
+         return axios.put(url, request_body, {
+            ...request_headers && {headers: request_headers}
+         });
+      },
+      num_retries
    });
 };
 
-const destroy = async({ url, request_headers }) => {
-   return async_executor.execute_with_retry(async () => {
-      return axios.delete(url, {
-         ...request_headers && { headers: request_headers },
-      });
+const destroy = async({ url, request_headers, num_retries }) => {
+   return async_executor.execute({
+      method: async () => {
+         return axios.delete(url, {
+            ...request_headers && { headers: request_headers },
+         });
+      },
+      num_retries
    });
 };
 
